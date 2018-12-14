3a87d19eae3f1f250e57b03b2b6ea29ae182c198
Harish Sunkerikar <h.sunkerikar@prowareness.nl>
2018-12-14 08:47:02 +0530

    Adding attributes and styling to webcomponents

--- tooltip.js
+++ tooltip.js
@@ -1,20 +1,29 @@
 class ToolTip extends HTMLElement {
   constructor() {
     super();
-    console.log("It is working");
     this._tootlTipContainer;
+    this._toolTipText = "Som dummy tooltip text";
   }
 
   connectedCallback() {
+    if (this.hasAttribute("text")) {
+      this._toolTipText = this.getAttribute("text");
+    }
+
     const toolTipIcon = document.createElement("span");
     toolTipIcon.textContent = " (?)";
     toolTipIcon.addEventListener("mouseenter", this._showToolTip.bind(this));
     toolTipIcon.addEventListener("mouseleave", this._hideToolTip.bind(this));
     this.appendChild(toolTipIcon);
+    this.style = "relative";
   }
   _showToolTip() {
     this._tootlTipContainer = document.createElement("div");
-    this._tootlTipContainer.textContent = "this is tooltip text";
+    this._tootlTipContainer.textContent = this._toolTipText;
+    this._tootlTipContainer.style.backgroundColor = "black";
+    this._tootlTipContainer.style.color = "white";
+    this._tootlTipContainer.style.position = "absolute";
+    this._tootlTipContainer.style.zIndex = 10;
     this.appendChild(this._tootlTipContainer);
   }
   _hideToolTip() {
